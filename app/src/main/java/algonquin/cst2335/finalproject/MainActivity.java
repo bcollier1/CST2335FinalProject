5b7ddf4e79a7ca0ef4052bc68fc82e920170fd4b
bcollier1 <brady_c_223@hotmail.com>
2023-07-18 20:32:34 -0400

    Implemented milestone 1, 4, 5, and 12. Next commit will include SharedPreferences, and merging the other branches.

--- app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -2,13 +2,23 @@ package algonquin.cst2335.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import algonquin.cst2335.finalproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bearGenerator.setOnClickListener(clk ->{
+            Intent bearPage = new Intent(MainActivity.this, BearGeneratorUI.class);
+            startActivity(bearPage);
+        });
     }
 }
