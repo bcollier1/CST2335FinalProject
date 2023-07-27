6110706e9759c51d2a2acdb0cdaa2fe07212b18e
bcollier1 <brady_c_223@hotmail.com>
2023-07-27 14:42:41 -0400

    Implemented the strings.xml to allow for multi language locale. Included toolbar with help menu. Next push will implement the Volley to retrieve data from the bear generator website.

--- app/src/main/java/algonquin/cst2335/finalproject/ui/BearGeneratorActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/ui/BearGeneratorActivity.java
@@ -7,8 +7,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -17,6 +20,10 @@ import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import algonquin.cst2335.finalproject.R;
 import algonquin.cst2335.finalproject.databinding.ActivityBearGeneratorUiBinding;
 
 public class BearGeneratorActivity extends AppCompatActivity {
@@ -37,13 +44,13 @@ public class BearGeneratorActivity extends AppCompatActivity {
         SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
         Context context = getApplicationContext();
+        setSupportActionBar(binding.toolBar);
         recyclerView = binding.recyclerView;
 
         binding.widthInput.setText(prefs.getString("Width",""));
         binding.heightInput.setText(prefs.getString("Height",""));
 
         uploadBTN = binding.newImage;
-        deleteBTN = binding.garbageButton;
 
         uploadBTN.setOnClickListener(clk ->{
             String width = binding.widthInput.getText().toString();
@@ -59,19 +66,6 @@ public class BearGeneratorActivity extends AppCompatActivity {
                 Toast toast = Toast.makeText(context, "Upload Failed!", Toast.LENGTH_SHORT);
                 toast.show();
             }
-        });
-
-        deleteBTN.setOnClickListener(clk ->{
-            AlertDialog.Builder builder = new AlertDialog.Builder(BearGeneratorActivity.this);
-            builder.setMessage("Do you want to delete this image?")
-                    .setTitle("Question:")
-                    .setNegativeButton("No", (((dialog, which) -> {})))
-                    .setPositiveButton("Yes", ((dialog, which) -> {
-                        Snackbar.make(imageView,"Image deleted!", Snackbar.LENGTH_LONG)
-                                .setAction("Undo", cl->{})
-                                .show();
-                    }))
-                    .create().show();
         });
 
         recyclerView.setAdapter(myAdapter = new RecyclerView.Adapter<MyRowHolder>() {
@@ -97,6 +91,66 @@ public class BearGeneratorActivity extends AppCompatActivity {
         });
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        String words = "";
+
+        if(item.getItemId() == R.id.airportPage){
+            Intent airTracker = new Intent(BearGeneratorActivity.this, AirportDisplayBoardActivity.class);
+            BearGeneratorActivity.this.startActivity(airTracker);
+        }
+
+        else if(item.getItemId() == R.id.currencyPage){
+            Intent airTracker = new Intent(BearGeneratorActivity.this, CurrencyActivity.class);
+            BearGeneratorActivity.this.startActivity(airTracker);
+        }
+
+        else if(item.getItemId() == R.id.triviaPage){
+            Intent airTracker = new Intent(BearGeneratorActivity.this, TriviaActivity.class);
+            BearGeneratorActivity.this.startActivity(airTracker);
+        }
+
+        else if(item.getItemId() == R.id.aboutBear){
+            String message = "Let's get started! In the toolbar you will see five different icons.\n" +
+                    "Airplane - This button is linked to the Airplane Tracker. It will switch you from this" +
+                    "current page to there, which you can track the current status of your airline.\n" +
+                    "Currency - This button is linked to the Currency Converter. This page will allow you to" +
+                    "input a currency of choice and convert to another countries local currency.\n" +
+                    "Trivia - This button is linked to the Trivia Game. You will be able to test your knowledge" +
+                    "with very skilled questions from a various of difficulty.\nTrash Can - Any image you "+
+                    "generated will be deleted.\nInfo - Provides information of how the interface of this " +
+                    "application works. On this page you will find two inputs; 'Width' and 'Height'. Enter "+
+                    "a desired resolution size, and the application will retrieve a bear image from <html page>. "+
+                    "The two buttons; 'Add' and 'Library' allows you to generate a new image and retrieve an "+
+                    "that was already generated.";
+            AlertDialog.Builder infoBuilder = new AlertDialog.Builder(BearGeneratorActivity.this);
+            infoBuilder.setMessage(message)
+                    .setTitle("Welcome to CST2335 Final Project!")
+                    .setNegativeButton("Close", ((dialog, cl) -> {
+                    }))
+
+                    .create().show();
+        }
+
+        else if(item.getItemId() == R.id.trashBin) {
+            words = "Test 2";
+        }
+
+        Toast.makeText(this, words, Toast.LENGTH_LONG).show();
+
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.bear_generator_menu, menu);
+
+        return true;
     }
 
     protected static class MyRowHolder extends RecyclerView.ViewHolder {
